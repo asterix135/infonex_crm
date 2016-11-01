7d01c76df64a2afcb3c0a8961fb4ddf35bbc1c45
asterix135 <chgraham@gmail.com>
2016-11-01 16:35:35 -0400

    javascript updates

--- crm/static/crm/javascript/crm_scripts.js
+++ crm/static/crm/javascript/crm_scripts.js
@@ -1,9 +1,9 @@
 // Toggles checkbox status in territory view for all individuals
 function checkAll() {
-  var masterBoxStatus = document.getElementById('checkAllBoxes').checked
-  checkboxes = document.getElementsByName('reflag');
-  for(var i=0; i<checkboxes.length; i++) {
-    checkboxes[i].checked = masterBoxStatus;
+  // var masterBoxStatus = document.getElementById('checkAllBoxes').checked
+  // checkboxes = document.getElementsByName('reflag');
+  // for(var i=0; i<checkboxes.length; i++) {
+  //   checkboxes[i].checked = masterBoxStatus;
   }
 }
 
--- crm/static/crm/javascript/jquery_scripts.js
+++ crm/static/crm/javascript/jquery_scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function()) {
-  alert('hello');
-}
+$(document).ready(function() {
+  // alert('hello');
+
+  $("#checkAllBoxes").click(function(){
+    var checked_status = this.checked;
+    $("input[name='reflag'").each(function(){
+      this.checked = checked_status;
+    });
+  });
+
+});

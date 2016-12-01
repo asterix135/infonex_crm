c869517e7b0ffaa4eef26d54ec1fce5859deeccd
asterix135 <chgraham@gmail.com>
2016-12-01 17:08:36 -0500

    started reorg of crm

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from django.http import HttpResponse
@@ -5,6 +6,7 @@ from django.http import HttpResponse
 # def index(request):
 #     return HttpResponse('This is the home page for all web apps')
 
+@login_required
 def index(request):
     user = request.user
     reg_permission_ok = (user.groups.filter(name='db_admin').exists() or

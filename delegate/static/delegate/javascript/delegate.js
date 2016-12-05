7e75320fa75c494297fcf1b4995cf9d2bbdbc0a1
asterix135 <chgraham@gmail.com>
2016-12-05 08:38:46 -0500

    delegate.js updated

--- delegate/static/delegate/javascript/delegate.js
+++ delegate/static/delegate/javascript/delegate.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
-
+  $('body').on('keyup', '#id_first_name,#id_last_name', function(){
+    var charsEntered = $('#id_first_name').val().length +
+      $('#id_last_name').val().length;  
+    // TODO: I think id_name might be repeated - check
+    charsEntered += $('#id_name').val().length;
+    console.log(charsEntered);
+  })
 
 });

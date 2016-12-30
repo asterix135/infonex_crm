86c38196c863561ffedb42dba580adb19097296c
asterix135 <chgraham@gmail.com>
2016-12-30 15:55:05 -0500

    updated search page ui

--- crm/static/crm/javascript/search.js
+++ crm/static/crm/javascript/search.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
 
   $('body').on('click', '#btn-toggle-adv-search', function(){
     console.log('triggered function');
-    if ($('#icon-toggle-adv-search').hasClass('glyphicon-chevron-down')) {
-      $('#icon-toggle-adv-search').removeClass('glyphicon-chevron-down');
-      $('#icon-toggle-adv-search').addClass('glyphicon-chevron-up');
+    if ($(this).hasClass('glyphicon-chevron-down')) {
+      $(this).removeClass('glyphicon-chevron-down');
+      $(this).addClass('glyphicon-chevron-up');
     } else {
-      $('#icon-toggle-adv-search').removeClass('glyphicon-chevron-up');
-      $('#icon-toggle-adv-search').addClass('glyphicon-chevron-down');
-    }
+      $(this).removeClass('glyphicon-chevron-up');
+      $(this).addClass('glyphicon-chevron-down');
+    };
+    $('#search-panel').toggle();
   });
 });

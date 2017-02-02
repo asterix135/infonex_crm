b3381e15e20314861d76da9fee7d0a8479acafb4
asterix135 <chgraham@gmail.com>
2017-02-02 14:55:44 -0500

    homepage changes

--- home/views.py
+++ home/views.py
@@ -1,10 +1,14 @@
+import datetime
+
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
-from django.http import HttpResponse
+from django.utils import timezone
 
-# def index(request):
-#     return HttpResponse('This is the home page for all web apps')
+from crm.models import Contact
+from . import mycharts
+
 
 @login_required
 def index(request):
@@ -13,5 +17,29 @@ def index(request):
                          user.groups.filter(name='registration').exists() or
                          user.groups.filter(name='management').exists() or
                          user.is_superuser)
-    context = {'reg_permission_ok': reg_permission_ok}
+    day_counts = {'Monday': 0,
+                  'Tuesday': 0,
+                  'Wednesday': 0,
+                  'Thursday': 0,
+                  'Friday': 0,
+                  'Today': 0}
+    today_contacts = Contact.objects.filter(
+        # author=user,
+        date_of_contact__date=datetime.datetime.today().date()
+    ).count()
+    day_counts['Today'] = today_contacts
+    context = {'reg_permission_ok': reg_permission_ok,
+               'day_counts': day_counts}
     return render(request, 'home/index.html', context)
+
+
+@login_required
+def recent_contact_chart(request):
+    user = request.user
+    today_contacts = Contact.objects.filter(
+        author=user,
+        date_of_contact__date=datetime.datetime.today().date()
+    )
+    for i in range (7):
+        if timezone.now():
+            pass

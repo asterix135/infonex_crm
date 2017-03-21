f001486fe5f428e7b8a35fd1498258d8e5a00833
asterix135 <chgraham@gmail.com>
2017-03-21 17:02:37 -0400

    bug fix

--- crm/views.py
+++ crm/views.py
@@ -1520,8 +1520,8 @@ def call_report(request):
             person = person + '<br/>' + contact.person.company
         date = Paragraph(str(contact.date_of_contact.date()), cell_style)
         person = Paragraph(person, cell_style)
-        notes = Paragraph(contact.notes, cell_style)
-        data.append([date, person, 'notes'])
+        notes = Paragraph(contact.notes.strip(), cell_style)
+        data.append([date, person, notes])
         table = Table(data, [inch, 3 * inch, 4.5 * inch])
         table.setStyle(TableStyle([('VALIGN', (0,0), (-1, -1), 'TOP')]))
         report_details.append(table)

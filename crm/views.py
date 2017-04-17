5268c53b3ea43850995033136dd41167f9b44940
asterix135 <chgraham@gmail.com>
2017-04-17 16:40:21 -0400

    bug fix

--- crm/views.py
+++ crm/views.py
@@ -606,10 +606,10 @@ def search(request):
         search_list = Person.objects.filter(query)
 
     # execute advanced search
-    elif search_name not in ('', None) or \
-        search_title not in ('', None) or \
-        search_company not in ('', None) \
-        or search_prov not in ('', None):
+    elif (search name and search_name not in ('', None)) or \
+        (search_title and search_title not in ('', None)) or \
+        (search_company and search_company not in ('', None)) or \
+        (search_prov and search_prov not in ('', None)):
 
         search_list = Person.objects.filter(name__icontains=search_name,
                                             title__icontains=search_title,

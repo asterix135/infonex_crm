4fdff4a6298e11b104e182d549bd94b7e9542f0c
asterix135 <chgraham@gmail.com>
2017-07-19 17:48:10 -0400

    progress on marketing page

--- marketing/views.py
+++ marketing/views.py
@@ -1,4 +1,15 @@
 from django.views.generic import TemplateView
+from crm.models import Person
+from crm.constants import GEO_CHOICES, CAT_CHOICES, DIV_CHOICES
 
 class Index(TemplateView):
     template_name = 'marketing/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        records = Person.objects.all()[:100]
+        context['records'] = records
+        context['geo_choices'] = GEO_CHOICES
+        context['cat_choices'] = CAT_CHOICES
+        context['div_choices'] = DIV_CHOICES
+        return context

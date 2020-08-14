b74fc418b0c7d51864ded5ba3e3dfbabde0a6f24
Christopher Graham <asterix135@users.noreply.github.com>
2020-08-14 08:54:32 -0400

    javascript change

--- static/crm/javascript/beta_territory.js
+++ static/crm/javascript/beta_territory.js
@@ -2,12 +2,14 @@
 $(document).ready(function() {
   $('body').on('change', '#id_division1', function(){
     let newDivision = $(this).val();
-    console.log(newDivision);
+    // if (newDivision='')
+    // console.log(newDivision);
   });
 
   $('body').on('change', '#id_division2', function(){
     let newDivision2 = $(this).val();
-    console.log(newDivision2);
+    // console.log(newDivision2);
   });
+
 
 });
